81070a73f3d3503be77b79fb6493e542da1c65fd
Ashutosh A <96434947+Ashutosh-Ardu@users.noreply.github.com>
2022-09-25 23:52:58 +0530

    Commit-3

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function createItems(icon) {
   const top = randomNumberBetween(0, 50)
   const size = top / 5 + 1
   img.classList.add("items")
-  img.src = `./imgs/${icon}.svg`
+  img.src = `./imgs/${icon}.png`
   img.style.width = `${size}vmin`
   img.style.height = `${size}vmin`
   img.style.top = `${top}vh`
